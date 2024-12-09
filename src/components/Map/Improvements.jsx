735d2df33fad751a3a57f66897f7ff8f1d43dc95
kenancross <kenancross@gmail.com>
2024-12-08 20:23:54 -0500

    Added improvements.jsx

--- src/components/Map/Improvements.jsx
+++ src/components/Map/Improvements.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
 export const Improvements = {
-    house: {
-        costs: { lumber: 5, grain: 5, water: 5, sheep: 1 },
-        benefits: {people: 5}
-    },
-    field: {
-        costs: { people: 1, water: 2 },
-        benefits: {grain: 10}
-    },
-    pasture: {
-        costs: { people: 1, grain: 2, water: 2 },
-        benefits: {sheep: 5}
-    },
-    lumberMill: {
-        costs: { people: 1 },
-        benefits: {lumber: 10}
-    },
-    well: {
-        costs: { people: 1, lumber: 2 },
-        benefits: {water: 10}
-    }
+	house: {
+		costs: { lumber: 5, grain: 5, water: 5, sheep: 1, people: 0 },
+		benefits: { people: 5 },
+	},
+	field: {
+		costs: { lumber: 0, grain: 0, water: 2, sheep: 0, people: 1 },
+		benefits: { grain: 10 },
+	},
+	pasture: {
+		costs: { lumber: 0, grain: 2, water: 2, sheep: 0, people: 1 },
+		benefits: { sheep: 5 },
+	},
+	lumberMill: {
+		costs: { lumber: 0, grain: 0, water: 0, sheep: 0, people: 0 },
+		benefits: { lumber: 10 },
+	},
+	well: {
+		costs: { lumber: 2, grain: 0, water: 0, sheep: 0, people: 1 },
+		benefits: { water: 10 },
+	},
 };
 
