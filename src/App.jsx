3518fbbeeb1fb9c134b35e348949d43e7dba3fcb
kenancross <kenancross@gmail.com>
2024-12-08 23:30:38 -0500

    Added work to properly calculate adding and subtracting resources.

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,58 @@
-import React, { useState } from 'react';
-import Map from './components/Map/Map';
-import ResourcesView from './components/ResourcesView/ResourcesView';
-import './styles/App.css';
-
-const initialResources = { //Object setting initial resources for game.
-  lumber: 5,
-  grain: 5,
-  water: 5,
-  sheep: 1,
-  people: 0,
+import React, { useState } from "react";
+import Map from "./components/Map/Map";
+import ResourcesView from "./components/ResourcesView/ResourcesView";
+import "./styles/App.css";
+import { Improvements } from "./components/Map/Improvements";
+const initialResources = {
+	//Object setting initial resources for game.
+	lumber: 5,
+	grain: 5,
+	water: 5,
+	sheep: 1,
+	people: 0,
 };
 
 const App = () => {
-  const [resources, setResources] = useState(initialResources); //useState for updating resources
-  const [improvements, setImprovements] = useState([]); //useState for setting improvements on each tile in the map.
+	const [resources, setResources] = useState(initialResources); //useState for updating resources
+	const [improvements, setImprovements] = useState([]); //useState for setting improvements on each tile in the map.
 
-  const addImprovement = (improvement) => { //passes improvement to tile that user clicked.
-    setImprovements([...improvements, improvement]); //passes in array of improvements up on line 16.
-  };
+	const addImprovement = (improvement) => {
+		//passes improvement to tile that user clicked.
+		setImprovements([...improvements, improvement]); //passes in array of improvements up on line 16.
+		console.log([improvement.type]);
+		const costs = Object.fromEntries(
+			Object.entries(Improvements[improvement.type].costs).map(
+				([key, value]) => [key, -value]
+			)
+		); //Negates the costs from the Improvements object to properly subtract values from the Resources.
+		const benefits = Improvements[improvement.type].benefits; //Access the benefits from adding the Improvemtns
+		updateResources(costs); //Subtract costs
+		updateResources(benefits); //Add benefits
+	};
 
-  const updateResources = (resourceChanges) => { // calls dialog for updating resource when user clicks a tile containing a resource
-    setResources((prev) =>
-      Object.fromEntries(
-        Object.entries(prev).map(([key, value]) => [
-          key,
-          value + (resourceChanges[key] || 0),
-        ])
-      )
-    );
-  };
+	const updateResources = (resourceChanges) => {
+		// calls dialog for updating resource when user clicks a tile containing a resource
+		setResources((prev) =>
+			Object.fromEntries(
+				Object.entries(prev).map(([key, value]) => [
+					key,
+					value + (resourceChanges[key] || 0),
+				])
+			)
+		);
+	};
 
-  return (
-    <div className="app">
-      <ResourcesView resources={resources} />
-      <Map
-        improvements={improvements}
-        addImprovement={addImprovement}
-        updateResources={updateResources}
-        resources={resources}
-      />
-    </div>
-  );
+	return (
+		<div className='app'>
+			<ResourcesView resources={resources} />
+			<Map
+				improvements={improvements}
+				addImprovement={addImprovement}
+				updateResources={updateResources}
+				resources={resources}
+			/>
+		</div>
+	);
 };
 
 export default App;
--- src/components/Map/Improvements.jsx
+++ src/components/Map/Improvements.jsx
@@ -3,23 +3,23 @@ import React from "react";
 export const Improvements = {
 	house: {
 		costs: { lumber: 5, grain: 5, water: 5, sheep: 1, people: 0 },
-		benefits: { people: 5 },
+		benefits: { lumber: 0, grain: 0, water: 0, sheep: 0, people: 5 },
 	},
 	field: {
 		costs: { lumber: 0, grain: 0, water: 2, sheep: 0, people: 1 },
-		benefits: { grain: 10 },
+		benefits: { lumber: 0, grain: 10, water: 0, sheep: 0, people: 0 },
 	},
 	pasture: {
 		costs: { lumber: 0, grain: 2, water: 2, sheep: 0, people: 1 },
-		benefits: { sheep: 5 },
+		benefits: { lumber: 0, grain: 0, water: 0, sheep: 5, people: 0 },
 	},
 	lumberMill: {
-		costs: { lumber: 0, grain: 0, water: 0, sheep: 0, people: 0 },
-		benefits: { lumber: 10 },
+		costs: { lumber: 0, grain: 0, water: 0, sheep: 0, people: 1 },
+		benefits: { lumber: 10, grain: 0, water: 0, sheep: 0, people: 0 },
 	},
 	well: {
 		costs: { lumber: 2, grain: 0, water: 0, sheep: 0, people: 1 },
-		benefits: { water: 10 },
+		benefits: { lumber: 0, grain: 0, water: 10, sheep: 0, people: 0 },
 	},
 };
 
