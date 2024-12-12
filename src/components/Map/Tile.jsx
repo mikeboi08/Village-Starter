e82dc10dc04d38c9e848b0e78a96a49aebece127
Tameika Thompson <meekmeek92@MacBook-Pro-82.local>
2024-12-12 18:05:06 -0500

    copied in Kens code for Tile.jsx

--- src/components/Map/Tile.jsx
+++ src/components/Map/Tile.jsx
@@ -1,36 +1,53 @@
 import React, { useState } from "react";
 import AddImprovementDialog from "./AddImprovementDialog";
+import EditImprovementDialog from "./EditImprovementDialog";
+import { Improvements } from "./Improvements";
 import "./styles/Tile.css";
+import Icon from "./Icon";
 
+const Tile = ({
+    index,
+    improvement,
+    addImprovement,
+    updateResources,
+    resources,
+}) => {
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-const Tile = ({ index, improvement, addImprovement, resources }) => {
-  const [isDialogOpen, setDialogOpen] = useState(false);
+    // Handle opening the dialog
+    const handleOpenDialog = () => {
+        setIsDialogOpen(!isDialogOpen);
+    };
 
-  return (
-    <div className="tile" onClick={() => setDialogOpen(true)}>
-      {improvement ? (
-        <div className="tile-improvement">
-          {/* Display the improvement's icon */}
-          <img src={improvement.icon} alt={improvement.type} className="tile-icon" />
-          {/* Display the improvement's level */}
-          <div className="tile-level">Level {improvement.level}</div>
+    // Handle closing the dialog box
+    const handleCloseDialog = () => {
+        setIsDialogOpen(!isDialogOpen);
+    };
+    
+
+    return (
+        <div className='tile' onClick={() => handleOpenDialog(true)}>
+            {improvement
+                ? isDialogOpen && (
+                        <EditImprovementDialog
+                            improvement={improvement}
+                            updateResources={updateResources}
+                            resources={resources}
+                            closeDialog={handleCloseDialog} // This is passed to EditImprovementDialog
+                        />
+                  )
+                : isDialogOpen && (
+                        <AddImprovementDialog
+                            index={index}
+                            addImprovement={addImprovement}
+                            resources={resources}
+                            onClose={handleCloseDialog} // Pass handleCloseDialog to AddImprovementDialog as onClose
+                        />
+                  )}
+            <Icon improvement={improvement} />
         </div>
-      ) : (
-        isDialogOpen && (
-          <AddImprovementDialog
-            index={index}
-            addImprovement={addImprovement}
-            resources={resources}
-            closeDialog={() => setDialogOpen(false)}
-          />
-        )
-      )}
-    </div>
-  );
+    );
 };
-
-
-
 
 export default Tile;
 
